471f41a0e7a3267693addf99dee17f09da86b791
ingiebaho <47155701+ingiebaho@users.noreply.github.com>
2019-03-03 23:55:20 +0400

    Update client.js

--- simpleConnectedDeviceAssignment/client.js
+++ simpleConnectedDeviceAssignment/client.js
@@ -1,1 +1,12 @@
-
+const {request} = require("http");
+//configures the request, telling Node what server to talk to, what path to request from that server, and which method to use
+let requestStream = request({
+  hostname: "eloquentjavascript.net",
+  path: "/20_node.html",
+  method: "GET",
+  headers: {Accept: "text/html"}
+}, response => {  //function that should be called when a response comes in
+  console.log("Server responded with status code",
+              response.statusCode);
+});
+requestStream.end();
