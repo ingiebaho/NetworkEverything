0cc37d016d5dcc1e3c3656824932b82e50c5961a
ingiebaho <47155701+ingiebaho@users.noreply.github.com>
2019-03-05 11:31:55 +0400

    added more comments

--- simpleConnectedDeviceAssignment/server.js
+++ simpleConnectedDeviceAssignment/server.js
@@ -1,4 +1,4 @@
-const {createServer} = require("http");
+const {createServer} = require("http");   //html header 
 let server = createServer((request, response) => {    //called everytime a client connects to server 
   response.writeHead(200, {"Content-Type": "text/html"}); //response headers
   //actual message being sent, I want to put data here from the humidity sensor
@@ -7,5 +7,5 @@ let server = createServer((request, response) => {    //called everytime a clien
     <p>You asked for <code>${request.url}</code></p>`);
   response.end();   //signals end of response 
 });
-server.listen(8000);
+server.listen(8000);  //defines which port is being used for communication 
 console.log("Listening! (port 8000)");
