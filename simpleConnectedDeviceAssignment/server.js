ff3c82c20b16935837f6a7ffd607e47e5ca13d2d
ingiebaho <47155701+ingiebaho@users.noreply.github.com>
2019-03-10 19:01:37 +0400

    Update server.js

--- simpleConnectedDeviceAssignment/server.js
+++ simpleConnectedDeviceAssignment/server.js
@@ -1,11 +1,85 @@
-const {createServer} = require("http");   //html header 
-let server = createServer((request, response) => {    //called everytime a client connects to server 
-  response.writeHead(200, {"Content-Type": "text/html"}); //response headers
-  //actual message being sent, I want to put data here from the humidity sensor
-  response.write(`
-    <h1>Hello!</h1>     
-    <p>You asked for <code>${request.url}</code></p>`);
-  response.end();   //signals end of response 
+/* 
+first find your IP address with
+
+	ifconfig
+
+then run this server with
+
+	node server.js
+
+connect using a browser to
+
+	http://192.168.1.4:8080
+
+*/
+
+	
+// For the HTTP server
+const http = require('http')
+const express = require('express'); // web server application
+const app = express();        // instantiate express server
+const webServer = http.Server(app);  // connects http library to server
+const HTTP_PORT=7000;
+
+// For the TCP server
+const ARDUINO_PORT=8001;
+
+
+/*
+	The HTTP server
+*/
+app.use(express.static('public'));  // find pages in public directory
+
+webServer.listen(HTTP_PORT, () => {
+  console.log('webServer: Listening at', webServer.address());
 });
-server.listen(8000);  //defines which port is being used for communication 
-console.log("Listening! (port 8000)");
+
+webServer.on('connection', (socket) => {
+  console.log("webServer: An HTTP client has connected")
+});
+
+
+/*
+	The TCP server
+*/
+
+// import the net module
+const { createServer } = require('net');
+
+// create and return a net.Server object
+const arduinoServer = createServer();
+
+arduinoServer.on('connection', (socket) => {
+  console.log("arduinoServer: A client has connected")
+
+  socket.on('data',function(data){
+    var numBytesRead = socket.bytesRead;
+    console.log('Bytes read: ' + 
+	    	numBytesRead + 
+	    	' buffer length: ' + 
+	    	data.length);
+  
+    //for (ii = 0; ii < data.length; ii++) {
+	  //console.log(data.readUInt8(ii));
+    //};
+	  console.log(data.toString('hex'));
+
+  }); // end of socket.on'data'
+
+  socket.on('end', data => {
+    console.log("arduinoServer: client has disconnected");
+  });
+}); // end of socket.on('connection')
+
+arduinoServer.listen(ARDUINO_PORT, () => {
+
+  var arduinoServerInfo = arduinoServer.address();
+
+  var arduinoServerInfoJson = JSON.stringify(arduinoServerInfo);
+
+  console.log('arduinoServer: Listening at : ' + arduinoServerInfoJson );
+
+  arduinoServer.on('error', function (error) {
+    console.error(JSON.stringify(error));
+ });
+});
