2bc05bb223b857ce27f49faccb2c2e33dabd6247
ingiebaho <47155701+ingiebaho@users.noreply.github.com>
2019-05-08 21:09:58 +0400

    updated tab opening function to open once

--- Final Project/src/public/client.js
+++ Final Project/src/public/client.js
@@ -33,10 +33,10 @@ socket.on('allOff', (data) => {
 
 
 function displayImage1 () {
-	var ctr = 0;
-	while(ctr = 0){
+	var tabOpen = 0;
+	if(tabOpen == 0){
 	window.open('https://www.google.com', '_blank');
-	ctr++;
+	tabOpen = 1;
 	}
 	//var myAudio1 = document.getElementById("myAudio1");
 
