f546f0cc326dff7a1ece340e26cce532f101a4af
ingiebaho <47155701+ingiebaho@users.noreply.github.com>
2019-05-12 15:27:35 +0400

    Update client.js

--- Final Project/src/public/client.js
+++ Final Project/src/public/client.js
@@ -8,6 +8,9 @@ var socket = io();
 var tabOpen1 = 0;
 var myWindow1;
 
+var tabOpen2 = 0;
+var myWindow2;
+
 
 socket.on('button1', (data) => {
 	console.log('received button 1 pressed event from webserver: ' + data);
@@ -45,10 +48,21 @@ function displayImage1 () {
 }
 
 function displayImage2 () {
+	if(tabOpen2 == 0){
+
+	myWindow2 = window.open("https://www.bing.com", "myWindow", "width=200,height=100");
+	tabOpen2 = 1;
+	}
 	
 }
 
 function displayImage3 () {
+	
+	if(tabOpen3 == 0){
+
+	myWindow3 = window.open("https://www.yahoo.com", "myWindow", "width=200,height=100");
+	tabOpen3 = 1;
+	}
 	
 	
 }
@@ -59,6 +73,18 @@ function hideTheImage () {
 		myWindow1.close();
 		console.log("closing tab");
 		tabOpen1 = 0;
+	}
+	
+	if(tabOpen2 == 1){
+		myWindow2.close();
+		console.log("closing tab");
+		tabOpen2 = 0;
+	}
+	
+	if(tabOpen3 == 1){
+		myWindow3.close();
+		console.log("closing tab");
+		tabOpen3 = 0;
 	}
 	
 	
