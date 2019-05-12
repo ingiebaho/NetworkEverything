3b4585ecd38241ceb0a32772596c07f90f7c69de
ingiebaho <47155701+ingiebaho@users.noreply.github.com>
2019-05-12 14:01:21 +0400

    Update client.js

--- Final Project/src/public/client.js
+++ Final Project/src/public/client.js
@@ -5,8 +5,14 @@ back to the web server
 
 var socket = io();
 
-var tabOpen = 0;
-var myWindow;
+var tabOpen1 = 0;
+var myWindow1;
+
+var tabOpen2 = 0;
+var myWindow2;
+
+var tabOpen3 = 0;
+var myWindow3;
 
 
 socket.on('button1', (data) => {
@@ -36,12 +42,12 @@ socket.on('allOff', (data) => {
 
 function displayImage1 () {
 	
-	if(tabOpen == 0){
+	if(tabOpen1 == 0){
 
-	myWindow = window.open("", "myWindow", "width=200,height=100");
- 	myWindow.document.write("<p>This is 'myWindow'</p>");
+	myWindow1 = window.open("", "myWindow", "width=200,height=100");
+ 	myWindow1.document.write("<p>This is 'myWindow1'</p>");
 		
-	tabOpen = 1;
+	tabOpen1 = 1;
 	}
 	//var myAudio1 = document.getElementById("myAudio1");
 
@@ -64,7 +70,16 @@ function displayImage1 () {
 }
 
 function displayImage2 () {
-	var myAudio1 = document.getElementById("myAudio1");
+	
+	if(tabOpen2 == 0){
+
+	myWindow2 = window.open("", "myWindow", "width=200,height=100");
+ 	myWindow2.document.write("<p>This is 'myWindow2'</p>");
+		
+	tabOpen2 = 1;
+	}
+	
+	/*var myAudio1 = document.getElementById("myAudio1");
 //myAudio1.src = "space.mp3";
 
 var myAudio2 = document.getElementById("myAudio2");
@@ -80,11 +95,20 @@ var myAudio3 = document.getElementById("myAudio3");
 	placeholder.style.opacity = 100;
 	myAudio1.pause();
 	myAudio2.play();
-	myAudio3.pause();
+	myAudio3.pause();*/
 }
 
 function displayImage3 () {
-	var myAudio1 = document.getElementById("myAudio1");
+	
+	if(tabOpen3 == 0){
+
+	myWindow3 = window.open("", "myWindow", "width=200,height=100");
+ 	myWindow3.document.write("<p>This is 'myWindow3'</p>");
+		
+	tabOpen3 = 1;
+	}
+	
+	/*var myAudio1 = document.getElementById("myAudio1");
 //myAudio1.src = "space.mp3";
 
 var myAudio2 = document.getElementById("myAudio2");
@@ -100,16 +124,24 @@ var myAudio3 = document.getElementById("myAudio3");
 	placeholder.style.opacity = 100;
 	myAudio1.pause();
 	myAudio2.pause();
-	myAudio3.play(); 
+	myAudio3.play(); */
 }
 
 function hideTheImage () {
 	
-	tabOpen = 0;
-	myWindow.close();
-	console.log("closing tab");
+	tabOpen1 = 0;
+	myWindow1.close();
+	console.log("closing tab1");
 	
-	var myAudio1 = document.getElementById("myAudio1");
+	tabOpen2 = 0;
+	myWindow2.close();
+	console.log("closing tab2");
+	
+	tabOpen3 = 0;
+	myWindow3.close();
+	console.log("closing tab3");
+	
+	/*var myAudio1 = document.getElementById("myAudio1");
 //myAudio1.src = "space.mp3";
 
 var myAudio2 = document.getElementById("myAudio2");
@@ -123,7 +155,7 @@ var myAudio3 = document.getElementById("myAudio3");
 	
 	myAudio1.pause();
 	myAudio2.pause();
-	myAudio3.pause();  
+	myAudio3.pause();  */
 	
 }
 
